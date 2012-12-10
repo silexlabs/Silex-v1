c7ca16d5a55f32fc22c37f997b9f42b3687e892d
Alex Hoyau <a.hoyau@silexlabs.org>
2012-12-10 16:30:13 +0100

    updated adress of the latest version script

--- framework/build-haxe-injection.sh
+++ framework/build-haxe-injection.sh
@@ -1,3 +1,3 @@
-/Applications/mtasc/mtasc ./as2/org/silex/core/Api.as ./as2/org/silex/link/HaxeLink.as ./as2/org/silex/ui/tools/RegionTool.as -swf bin/silexApiAs2Lib.swf -header 800:600:20 -cp ./as2/
+sudo /Applications/mtasc/mtasc ./as2/org/silex/core/Api.as ./as2/org/silex/link/HaxeLink.as ./as2/org/silex/ui/tools/RegionTool.as -swf bin/silexApiAs2Lib.swf -header 800:600:20 -cp ./as2/
 haxe build.hxml
 cp bin/CodeInjection.swf ../silex_server/silex.swf
